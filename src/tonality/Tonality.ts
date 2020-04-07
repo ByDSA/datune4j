1cf4e4d8da822d1e41fb706f05081c10877c8163
danisales <dsa21may@gmail.com>
2020-04-07 20:05:21 +0200

    Use DiatonicAlt instead of Chromatic

--- src/tonality/Tonality.ts
+++ src/tonality/Tonality.ts
@@ -1,57 +1,69 @@
 import { Chromatic } from '../degrees/Chromatic';
 import { Scale } from './Scale';
 import { ChromaticUtils } from '../degrees/ChromaticUtils';
+import { DiatonicAlt } from 'degrees/DiatonicAlt';
+import { Diatonic } from 'index';
+import { DiatonicUtils } from 'degrees/DiatonicUtils';
 
 export class Tonality {
-    public static C = Tonality.from(Chromatic.C, Scale.MAJOR);
-    public static CC = Tonality.from(Chromatic.CC, Scale.MAJOR);
-    public static D = Tonality.from(Chromatic.D, Scale.MAJOR);
-    public static DD = Tonality.from(Chromatic.DD, Scale.MAJOR);
-    public static E = Tonality.from(Chromatic.E, Scale.MAJOR);
-    public static F = Tonality.from(Chromatic.F, Scale.MAJOR);
-    public static FF = Tonality.from(Chromatic.FF, Scale.MAJOR);
-    public static G = Tonality.from(Chromatic.G, Scale.MAJOR);
-    public static GG = Tonality.from(Chromatic.GG, Scale.MAJOR);
-    public static A = Tonality.from(Chromatic.A, Scale.MAJOR);
-    public static B = Tonality.from(Chromatic.B, Scale.MAJOR);
+    public static C = Tonality.from(DiatonicAlt.C, Scale.MAJOR);
+    public static CC = Tonality.from(DiatonicAlt.CC, Scale.MAJOR);
+    public static D = Tonality.from(DiatonicAlt.D, Scale.MAJOR);
+    public static DD = Tonality.from(DiatonicAlt.DD, Scale.MAJOR);
+    public static E = Tonality.from(DiatonicAlt.E, Scale.MAJOR);
+    public static F = Tonality.from(DiatonicAlt.F, Scale.MAJOR);
+    public static FF = Tonality.from(DiatonicAlt.FF, Scale.MAJOR);
+    public static G = Tonality.from(DiatonicAlt.G, Scale.MAJOR);
+    public static GG = Tonality.from(DiatonicAlt.GG, Scale.MAJOR);
+    public static A = Tonality.from(DiatonicAlt.A, Scale.MAJOR);
+    public static B = Tonality.from(DiatonicAlt.B, Scale.MAJOR);
 
-    public static Cm = Tonality.from(Chromatic.C, Scale.MINOR);
-    public static CCm = Tonality.from(Chromatic.CC, Scale.MINOR);
-    public static Dm = Tonality.from(Chromatic.D, Scale.MINOR);
-    public static DDm = Tonality.from(Chromatic.DD, Scale.MINOR);
-    public static Em = Tonality.from(Chromatic.E, Scale.MINOR);
-    public static Fm = Tonality.from(Chromatic.F, Scale.MINOR);
-    public static FFm = Tonality.from(Chromatic.FF, Scale.MINOR);
-    public static Gm = Tonality.from(Chromatic.G, Scale.MINOR);
-    public static GGm = Tonality.from(Chromatic.GG, Scale.MINOR);
-    public static Am = Tonality.from(Chromatic.A, Scale.MINOR);
-    public static Bm = Tonality.from(Chromatic.B, Scale.MINOR);
+    public static Cm = Tonality.from(DiatonicAlt.C, Scale.MINOR);
+    public static CCm = Tonality.from(DiatonicAlt.CC, Scale.MINOR);
+    public static Dm = Tonality.from(DiatonicAlt.D, Scale.MINOR);
+    public static DDm = Tonality.from(DiatonicAlt.DD, Scale.MINOR);
+    public static Em = Tonality.from(DiatonicAlt.E, Scale.MINOR);
+    public static Fm = Tonality.from(DiatonicAlt.F, Scale.MINOR);
+    public static FFm = Tonality.from(DiatonicAlt.FF, Scale.MINOR);
+    public static Gm = Tonality.from(DiatonicAlt.G, Scale.MINOR);
+    public static GGm = Tonality.from(DiatonicAlt.GG, Scale.MINOR);
+    public static Am = Tonality.from(DiatonicAlt.A, Scale.MINOR);
+    public static Bm = Tonality.from(DiatonicAlt.B, Scale.MINOR);
 
-    private notes: Chromatic[] = [];
+    private notes: DiatonicAlt[] = [];
 
-    private constructor(private root: Chromatic, private scale: Scale) {
+    private constructor(private root: DiatonicAlt, private scale: Scale) {
         this.calculateNotes();
     }
 
-    public static from(root: Chromatic, scale: Scale) {
+    public static fromChromatic(chromaticRoot: Chromatic, scale: Scale) {
+        let root = DiatonicAlt.fromChromatic(chromaticRoot);
+        return Tonality.from(root, scale);
+    }
+
+    public static from(root: DiatonicAlt, scale: Scale) {
         return new Tonality(root, scale);
     }
 
     private calculateNotes(): void {
         this.notes.push(this.root);
 
-        let lastNote = this.root;
+        let lastNote = ChromaticUtils.fromDiatonicAlt(this.root);
+        let lastDiatonic = this.root.diatonic;
         let i = 0;
         for (let n of this.scale.getIntervals()) {
             if (i >= this.scale.getIntervals().length)
                 continue;
             lastNote += n;
             lastNote %= ChromaticUtils.NUMBER;
-            this.notes.push(lastNote);
+            lastDiatonic++;
+            lastDiatonic %= DiatonicUtils.NUMBER;
+            let note = DiatonicAlt.fromChromatic(lastNote, lastDiatonic);
+            this.notes.push(note);
         }
     }
 
-    getRoot(): Chromatic {
+    getRoot(): DiatonicAlt {
         return this.root;
     }
 
@@ -59,7 +71,7 @@ export class Tonality {
         return this.scale;
     }
 
-    getNotes(): Chromatic[] {
+    getNotes(): DiatonicAlt[] {
         return Array.from(this.notes);
     }
 }
