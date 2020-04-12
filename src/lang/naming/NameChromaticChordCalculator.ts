f1c35f0ae1f928fa3887e1d0db90d336cbe351ec
danisales <dsa21may@gmail.com>
2020-04-12 18:07:33 +0200

    Use only Chromatic

--- src/lang/naming/NameChromaticChordCalculator.ts
+++ src/lang/naming/NameChromaticChordCalculator.ts
@@ -1,20 +1,20 @@
+import { Chromatic } from '../../degrees/Chromatic';
 import { ChromaticChord } from '../../chords/chromatic/ChromaticChord';
 import { ChromaticChordPattern } from '../../chords/chromatic/ChromaticChordPattern';
-import { DiatonicAlt } from '../../degrees/DiatonicAlt';
 import { IntervalChromatic } from '../../interval/IntervalChromatic';
 
 export class NameChromaticChordCalculator {
     private pattern: ChromaticChordPattern;
     private inversion: number;
 
-    constructor(private chord: ChromaticChord, private rootDiatonicAlt: DiatonicAlt) {
+    constructor(private chord: ChromaticChord) {
     }
 
     public get(): string {
         this.pattern = ChromaticChordPattern.from(this.chord);
         this.inversion = this.chord.inversionNumber;
 
-        return this.rootDiatonicAlt.toString() + this.pattern.toStringShort() + this.inversionName();
+        return this.chord.root.toString() + this.pattern.toStringShort() + this.inversionName();
     }
 
     private inversionName(): string {
@@ -26,10 +26,10 @@ export class NameChromaticChordCalculator {
         return str;
     }
 
-    private getInversionDiatonicAlt(): DiatonicAlt {
+    private getInversionDiatonicAlt(): Chromatic {
         let semis: number = this.pattern.values[this.inversion];
         let intervalDiatonic: number = this.pattern.diatonicChordPattern.values[this.inversion];
         let intervalChromatic = IntervalChromatic.fromSemisAndNotes(semis, intervalDiatonic);
-        return this.rootDiatonicAlt.getShifted(intervalChromatic);
+        return Chromatic.fromInt(this.chord.root.intValue + intervalChromatic.semis);
     }
 }
